83b9d4f661e70c9baa423bad1404b8e3092ff343
Fazri Isalm <fazriislam.work@gmail.com>
2022-05-08 03:35:40 +0600

    Complete Authentication System

--- src/Pages/Authentication/SignIn/SignIn.js
+++ src/Pages/Authentication/SignIn/SignIn.js
@@ -2,7 +2,7 @@ import { async } from '@firebase/util';
 import React from 'react';
 import { Button, Form, Spinner } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import SocialSingUp from '../SocialSingUp/SocialSingUp';
 
@@ -42,19 +42,21 @@ const SignIn = () => {
             <Form onSubmit={handleSignIn}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
-                    <p className='text-danger'><small>{error?.message}</small></p>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" />
-                    <p className='text-danger'><small>{error?.message}</small></p>
+                    <Form.Control type="password" name='password' placeholder="Password" required/>
                 </Form.Group>
                 <Button variant="primary" type="submit" className='w-50 mx-auto btn btn-primary mt-2'>
                     Submit
                 </Button>
             </Form>
+            <div>
+                <p>Don't have any account? <Link className='text-primary pe-auto text-decoration-none' to='/signUp'>Sign Up</Link>
+                </p>
+            </div>
             <div>
                 <SocialSingUp />
             </div>
--- src/Pages/Authentication/SipnUp/SignUp.js
+++ src/Pages/Authentication/SipnUp/SignUp.js
@@ -1,36 +1,50 @@
 import { async } from '@firebase/util';
 import React, { useState } from 'react';
 import { Button, Form, Spinner } from 'react-bootstrap';
-import { useCreateUserWithEmailAndPassword, useSendEmailVerification  } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
-import { ToastContainer, toast } from 'react-toastify';
 import SocialSingUp from '../SocialSingUp/SocialSingUp';
 
 const SignUp = () => {
     const [agree, setAgree] = useState(false);
+    const [passError, setPassError] = useState('');
 
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
-        error,
+        error
     ] = useCreateUserWithEmailAndPassword(auth);
     const [sendEmailVerification, sending] = useSendEmailVerification(auth);
     const navigate = useNavigate();
-    
-    if (loading || sending) <Spinner animation='border' variant='primary'></Spinner>
+
+    if (loading || sending) {
+        return <Spinner animation='border' variant='primary'></Spinner>
+    }
 
     if (error) {
         return;
     }
-// --------------- handler function
-    const handleRegistration = async e =>{
+
+    // --------------- handler function
+    const handleRegistration = async e => {
         e.preventDefault();
 
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const ConfirmPassword = e.target.ConfirmPassword.value;
 
+        if (password.length < 8) {
+            const short = <p>Password should contain at least 8 character</p>
+            setPassError(short);
+            return;
+        }
+        if (password !== ConfirmPassword) {
+            const differentPass = <p>Password Doesn't match</p>
+            setPassError(differentPass);
+            return;
+        }
         await createUserWithEmailAndPassword(email, password);
         await sendEmailVerification(email);
         navigate('/');
@@ -43,22 +57,29 @@ const SignUp = () => {
             <Form onSubmit={handleRegistration}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
-                    <p className='text-danger'><small>{error?.message}</small></p>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
                 </Form.Group>
+
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" />
-                    <p className='text-danger'><small>{error?.message}</small></p>
+                    <Form.Control type="password" name='password' placeholder="Password" required/>
                 </Form.Group>
+                <p className='text-danger'><small>{passError}</small></p>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" name='ConfirmPassword' placeholder="Password" required/>
+                </Form.Group>
+                    <p className='text-danger'><small>{passError}</small></p>
+
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check onClick={() => setAgree(!agree)} type="checkbox" label="Accept terms and condition" />
                 </Form.Group>
-                <Button disabled={!agree} onClick={()=>{toast('Successfully Sing Up')}} variant="primary" type="submit" className='w-50 mx-auto btn btn-primary mt-2'>
+
+                <Button disabled={!agree} variant="primary" type="submit" className='w-50 mx-auto btn btn-primary mt-2'>
                     Submit
                 </Button>
-            <ToastContainer />
             </Form>
             <div>
                 <p>Already have a account? <Link className='text-primary pe-auto text-decoration-none' to='/signIn'>Sign In</Link>
