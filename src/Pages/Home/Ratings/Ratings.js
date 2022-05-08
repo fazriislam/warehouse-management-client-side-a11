2e7ce342d6c4ce657209ffe1f0bde00b8165b1a9
Fazri Isalm <fazriislam.work@gmail.com>
2022-05-08 23:28:21 +0600

    Solve Review Button

--- src/Pages/Home/Ratings/Ratings.js
+++ src/Pages/Home/Ratings/Ratings.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import Rating from './Rating/Rating';
 
 const Ratings = () => {
     const [ratings, setRatings] = useState([]);
+    const navigate=useNavigate();
 
     useEffect(() => {
         fetch('https://sheltered-springs-86908.herokuapp.com/rating')
             .then(res => res.json())
             .then(data => setRatings(data.slice(0, 3)))
     }, [ratings]);
+
+    const navigateToAllReview = ()=>{
+        navigate('/allReview');
+    }
 
     return (
         <div className='w-75 mt-3 mx-auto bg-danger p-3'>
@@ -21,7 +27,7 @@ const Ratings = () => {
                     />)
                 }
             </div>
-            <Button className='bg-dark text-white'>See All Review</Button>
+            <Button onClick={navigateToAllReview} className='bg-dark text-white'>See All Review</Button>
         </div>
     );
 };
